06d461ee59c44b9ab01d5dd2a525cc337cf4df24
Jeph Do <jdo@solarcity.com>
2014-08-24 09:57:43 -0700

    add authentication via email

--- flasky/__init__.py
+++ flasky/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.login import LoginManager
 from flask.ext.mail import Mail
 from flask.ext.migrate import Migrate
 from flask.ext.moment import Moment
@@ -9,6 +10,11 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from config import config
 
 bootstrap = Bootstrap()
+
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "auth.login"
+
 mail = Mail()
 migrate = Migrate()
 moment = Moment()
@@ -21,6 +27,7 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     bootstrap.init_app(app)
+    login_manager.init_app(app)
     mail.init_app(app)
     migrate.init_app(app, db)
     moment.init_app(app)
@@ -28,5 +35,8 @@ def create_app(config_name):
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
     return app
