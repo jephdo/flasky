bfd004391c8cad335fbad47e008ee0fc9910a0f6
Jeph Do <jephdo@gmail.com>
2014-08-31 01:00:06 -0700

    add ability to comment on posts

--- flasky/main/views.py
+++ flasky/main/views.py
@@ -2,11 +2,11 @@ from flask import render_template,  redirect, url_for, abort, flash, request, cu
 
 from flask.ext.login import current_user, login_required
 
-from .forms import PostForm, EditProfileForm, EditProfileAdminForm
+from .forms import PostForm, EditProfileForm, EditProfileAdminForm, CommentForm
 from . import main
 from .. import db
 from ..auth import admin_required, permission_required
-from ..models import User, Role, Post, Permission
+from ..models import User, Role, Post, Permission, Comment
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -49,10 +49,28 @@ def user(username):
     posts = user.posts.order_by(Post.timestamp.desc()).all()
     return render_template('user.html', user=user, posts=posts)
 
-@main.route("/post/<int:id>")
+@main.route("/post/<int:id>", methods=["GET", "POST"])
 def post(id):
-    p = Post.query.get_or_404(id)
-    return render_template("post.html", posts=[p])
+    post = Post.query.get_or_404(id)
+    form = CommentForm()
+
+    if form.validate_on_submit():
+        comment = Comment(body=form.body.data, post=post,
+                          author=current_user._get_current_object())
+        db.session.add(comment)
+        flash("Your comment has been published.")
+        redirect(url_for(".post", id=post.id, page=-1))
+    page = request.args.get("page", 1, type=int)
+    if page == -1:
+        page = (post.comments.count() - 1) / current_app.config["FLASKY_COMMENTS_PER_PAGE"] + 1
+
+    pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
+        page, per_page=current_app.config["FLASKY_COMMENTS_PER_PAGE"], error_out=False)
+
+    comments = pagination.items
+
+    return render_template("post.html", posts=[post], form=form,
+                           comments=comments, pagination=pagination)
 
 @main.route("/follow/<username>")
 @login_required
@@ -119,7 +137,6 @@ def followed_by(username):
     return render_template('followers.html', user=user, title="Followed by",
                            endpoint='.followed_by', pagination=pagination,
                            follows=follows)
-
 
 @main.route("/all")
 @login_required
@@ -198,3 +215,37 @@ def edit_profile_admin(id):
     form.location.data = user.location
     form.about_me.data = user.about_me
     return render_template('edit_profile.html', form=form, user=user)
+
+@main.route('/moderate')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def moderate():
+    page = request.args.get('page', 1, type=int)
+    pagination = Comment.query.order_by(Comment.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
+        error_out=False)
+
+    comments = pagination.items
+
+    return render_template('moderate.html', comments=comments,
+        pagination=pagination, page=page)
+
+@main.route('/moderate/enable/<int:id>')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def moderate_enable(id):
+    comment = Comment.query.get_or_404(id)
+    comment.disabled = False
+    db.session.add(comment)
+
+    return redirect(url_for('.moderate', page=request.args.get('page', 1, type=int)))
+
+@main.route('/moderate/disable/<int:id>')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def moderate_disable(id):
+    comment = Comment.query.get_or_404(id)
+    comment.disabled = True
+    db.session.add(comment)
+
+    return redirect(url_for('.moderate', page=request.args.get('page', 1, type=int)))
